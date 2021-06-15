1439fa2d9a177f962e2ca8a05008ed29912e4d38
abr-ya <gamer05@list.ru>
2021-06-15 14:35:57 +0300

    feat: Add nav-menu handler

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 const getCoord = (coordDeg) => ol.proj.transform(coordDeg, 'EPSG:4326', 'EPSG:3857');
 const change = (arr) => [arr[1], arr[0]];
+const googleCoord = [-25.785, 132.28]; // 47.24, 39.71 - дгту парк, -25.785, 132.28 - авст.
+const startCoord = getCoord(change(googleCoord));
 
 const init = () => {
-  const googleCoord = [-25.785, 132.28]; // 47.24, 39.71 - дгту парк, -25.785, 132.28 - авст.
   const map = new ol.Map({
     view: new ol.View({
-      center: getCoord(change(googleCoord)),
+      center: startCoord,
       zoom: 4,
     }),
     layers: [
@@ -56,7 +57,36 @@ const init = () => {
   map.addLayer(pinsLayer);
 
 
-  // Pin Click
+  // Create NavItems
+  const updateNav = () => {
+    const allFeat = pinsLayer.getSource().getFeatures();
+    allFeat.forEach(f => {
+      const city = f.values_.city;
+      const item = document.createElement('a');
+      item.innerHTML = `<i class='far fa-circle'></i>`;
+      item.setAttribute('title', city);
+      item.setAttribute('id', city);
+      nav.appendChild(item);
+    });
+
+    // nav clicks - navItems
+    const navItems = document.querySelectorAll('.nav > a');
+    const navItemClickHandler = (item) => {
+      const allFeat = pinsLayer.getSource().getFeatures();
+      const currentFeature = allFeat.find(f => f.values_.city === item.id);
+      itemClickHandler(currentFeature, item); // без фичи должно работать!
+    };
+
+    navItems.forEach(item => {
+      item.addEventListener('click', () => navItemClickHandler(item));
+    });
+  };
+  setTimeout(() => {
+    updateNav();
+  }, 100);
+
+
+  // Pin or Nav Click
   const nav = document.querySelector('.nav');
   const city = {
     text: document.querySelector('#cityname'),
@@ -64,30 +94,34 @@ const init = () => {
   };
   const mapView = map.getView();
 
+  // common switch function
   const itemClickHandler = (feature, el) => {
-    console.log(feature.values_.geometry.flatCoordinates);
-
     // change active nav element
     nav.querySelector('.active').classList.remove('active');
     el.classList.add('active');
 
     // change the view
-    mapView.animate(
-      {center: feature.values_.geometry.flatCoordinates},
-      {zoom: 5},
-    );
+    const center = feature ? feature.values_.geometry.flatCoordinates : startCoord;
+    const zoom = feature ? 5 : 4;
+    mapView.animate({center, zoom});
 
     // change features styles
     const allFeat = pinsLayer.getSource().getFeatures();
     allFeat.forEach(f => {
       f.setStyle(cityStyle(f));
     });
-    feature.setStyle(activeStyle(feature));
+    if (feature) feature.setStyle(activeStyle(feature));
 
     // change legend
-    city.text.innerHTML = `Active city is: ${feature.values_.city}`;
-    city.img.src = `./img/${feature.values_.city}.jpg`
+    if (feature) {
+      city.text.innerHTML = `Active city is: ${feature.values_.city}`;
+      city.img.src = `./img/${feature.values_.city}.jpg`
+    } else {
+      city.text.innerHTML = 'go to start page...';
+      city.img.src = './img/flag.jpg';
+    }
   };
+  // /common switch function
 
   // pin clicks
   map.addEventListener('click', (e) => {
@@ -98,7 +132,8 @@ const init = () => {
     });
   });
 
-  // nav clicks
+  // nav clicks - navItems
+  // переехала в часть с задержкой - после генерации
 };
 
 window.onload = init;
